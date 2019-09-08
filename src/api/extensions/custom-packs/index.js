817dca304735a859befe2c2cbae2c3c743477e2b
Fifciuu <filip.jdrasik@gmail.com>
2019-09-08 17:48:12 +0200

    Custom packs new endpoints

--- src/api/extensions/custom-packs/index.js
+++ src/api/extensions/custom-packs/index.js
@@ -59,7 +59,7 @@ module.exports = ({ config, db }) => {
 
   });
 
-  mcApi.post('/add/:storeCode', (req, res) => {
+  mcApi.post('/init/:storeCode', (req, res) => {
 
     if (!req.body.cartItem) {
 			return apiStatus(res, 'No cartItem element provided within the request body', 500)
@@ -76,7 +76,8 @@ module.exports = ({ config, db }) => {
     client.addMethods("packs", function(restClient) {
       var module = {};
 
-      module.update = function (customerToken, cartId, cartItem, adminRequest = false) {
+      // 1. We add the pack parent to the cart
+      module.initPack = function (customerToken, cartId, cartItem, adminRequest = false) {
         if (adminRequest) {
             return restClient.post('/carts/' + cartId + '/items?separate=1&pack_type=parent', { cartItem: cartItem });
         } else {
@@ -87,13 +88,79 @@ module.exports = ({ config, db }) => {
                 return restClient.post('/guest-carts/' + cartId + '/items?separate=1&pack_type=parent', { cartItem: cartItem });
             }
         }
-      } 
+      }
+
+      // 2. We add the pack packaging to the cart
+      module.addPackParent = function (customerToken, cartId, cartItem, packId, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.post('/carts/' + cartId + '/items?separate=1&pack_type=parent&pack_id=' + packId, { cartItem: cartItem });
+        } else {
+            if (customerToken && !isNaN(cartId)) {
+                return restClient.post('/carts/mine/items?separate=1&pack_type=parent&pack_id=' + packId, { cartItem: cartItem }, customerToken);
+            } else 
+            {
+                return restClient.post('/guest-carts/' + cartId + '/items?separate=1&pack_type=parent&pack_id=' + packId, { cartItem: cartItem });
+            }
+        }
+      }
+
       return module;
     });
 
     
-		client.packs.update(req.query.token, req.query.cartId ? req.query.cartId : null, req.body.cartItem).then((result) => {
-			apiStatus(res, result, 200);
+		client.packs.initPack(req.query.token, req.query.cartId ? req.query.cartId : null, req.body.cartItem).then((result) => {
+      
+      client.packs.addPackParent(req.query.token, req.query.cartId ? req.query.cartId : null, req.body.cartItem, result.item_id).then((result2) => {
+        apiStatus(res, result, 200);
+      }).catch(err => {
+			  apiStatus(res, err, 500);
+      })
+
+		}).catch(err => {
+			apiStatus(res, err, 500);
+		})
+
+  })
+
+  mcApi.post('/add/:packId/:storeCode', (req, res) => {
+
+    if (!req.body.cartItem) {
+			return apiStatus(res, 'No cartItem element provided within the request body', 500)
+		}
+
+    const client = Magento2Client({
+      ...config.magento2.api,
+      url:
+        config.magento2.api.url.replace("/rest", "/") +
+        req.params.storeCode +
+        "/rest"
+    });
+
+    client.addMethods("packs", function(restClient) {
+      var module = {};
+
+      // 3. We add childs to the parent
+      module.addPackChild = function (customerToken, cartId, cartItem, packId, adminRequest = false) {
+        if (adminRequest) {
+            return restClient.post('/carts/' + cartId + '/items?separate=1&pack_type=child&pack_id=' + packId, { cartItem: cartItem });
+        } else {
+            if (customerToken && !isNaN(cartId)) {
+                return restClient.post('/carts/mine/items?separate=1&pack_type=child&pack_id=' + packId, { cartItem: cartItem }, customerToken);
+            } else {
+                return restClient.post('/guest-carts/' + cartId + '/items?separate=1&pack_type=child&pack_id=' + packId, { cartItem: cartItem });
+            }
+        }
+      }
+
+
+      return module;
+    });
+
+    
+		client.packs.addPackChild(req.query.token, req.query.cartId ? req.query.cartId : null, req.body.cartItem, packId).then((result) => {
+      
+      apiStatus(res, result, 200);
+
 		}).catch(err => {
 			apiStatus(res, err, 500);
 		})
